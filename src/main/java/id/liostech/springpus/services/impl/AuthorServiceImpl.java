4e05c238e7aabec4813e2d19382a80902e5f45f4
Ibrahim hanif <ibrahimhanif@student.polindra.ac.id>
2021-11-19 15:39:13 +0700

    change readme

--- src/main/java/id/liostech/springpus/services/impl/AuthorServiceImpl.java
+++ src/main/java/id/liostech/springpus/services/impl/AuthorServiceImpl.java
@@ -4,6 +4,7 @@ import id.liostech.springpus.dto.request.AuthorCreateRequest;
 import id.liostech.springpus.entities.Author;
 import id.liostech.springpus.repositories.AuthorRepository;
 import id.liostech.springpus.services.AuthorService;
+import id.liostech.springpus.utils.ValidationUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,11 @@ public class AuthorServiceImpl implements AuthorService {
     private AuthorRepository authorRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private ValidationUtil validationUtil;
     @Override
     public Author create(AuthorCreateRequest authorCreateRequest) {
+        validationUtil.validate(authorCreateRequest);
         Author author = modelMapper.map(authorCreateRequest, Author.class);
         return authorRepository.save(author);
     }
